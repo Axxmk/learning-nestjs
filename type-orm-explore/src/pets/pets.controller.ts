de6533409cc885d47cc11ec41563fb2916ec1b40
Ms.MONTHARA KIATRUANGCHAI <monthara.k@mail.kmutt.ac.th>
2021-10-22 14:51:01 +0700

    update pet controller (return type)

--- type-orm-explore/src/pets/pets.controller.ts
+++ type-orm-explore/src/pets/pets.controller.ts
@@ -2,33 +2,34 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from 
 import { PetsService } from './pets.service';
 import { CreatePetDto } from './dto/create-pet.dto';
 import { UpdatePetDto } from './dto/update-pet.dto';
+import { Pet } from './entities/pet.entity';
 
 @Controller('pets')
 export class PetsController {
   constructor(private readonly petsService: PetsService) { }
 
   @Post()
-  create(@Body() createPetDto: CreatePetDto) {
+  create(@Body() createPetDto: CreatePetDto): Promise<Pet> {
     return this.petsService.createPet(createPetDto);
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<Pet[]> {
     return this.petsService.getAll();
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
+  findOne(@Param('id', ParseIntPipe) id: number): Promise<Pet> {
     return this.petsService.getOneById(id);
   }
 
   @Patch(':id')
-  update(@Param('id', ParseIntPipe) id: number, @Body() updatePetDto: UpdatePetDto) {
+  update(@Param('id', ParseIntPipe) id: number, @Body() updatePetDto: UpdatePetDto): Promise<Pet> {
     return this.petsService.updatePet(id, updatePetDto);
   }
 
   @Delete(':id')
-  remove(@Param('id', ParseIntPipe) id: number) {
+  remove(@Param('id', ParseIntPipe) id: number): Promise<Pet> {
     return this.petsService.removePet(id);
   }
 }
